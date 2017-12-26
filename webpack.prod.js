60b74fc80d0ccd544784583fba2098f72365296f
sangle <sanglewang@gmail.com>
2017-12-26 17:53:46 +0800

    [update]: webpack.prod.config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,25 +1,42 @@
 const webpack = require('webpack');
 const env = process.env.NODE_ENV;
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const theme = require('./src/theme.js');
 
 const config = {
-  entry: ['./src/index','webpack-hot-middleware/client'],
+  entry: {
+    vendor: ['react', 'prop-types', 'classnames', 'react-dom', 'react-router-dom'],
+    main: './src/index',
+  },
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name]-dev.js',
-    publicPath: '/static',
+    path: path.join(__dirname, 'app', 'public'),
+    filename: '[name]-[hash].js',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false, // remove all comments
+      },
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'], // 指定公共 bundle 的名字。
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
+    new ExtractTextPlugin("style.css"),
+    new HtmlWebpackPlugin({
+      title:'HNC-gene-database'
+    })
   ],
   devtool: false,
   module: {
-    loaders: [
-      {
+    loaders: [{
         test: /\.js$/,
         loaders: ['babel-loader'],
         exclude: /node_modules/,
@@ -27,21 +44,32 @@ const config = {
       },
       {
         test: /\.less$/,
-        loaders: ['style-loader', 'css-loader?modules&localIdentName=[name]--[local]--[hash:base64:5]', 'less-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader?modules&localIdentName=[name]--[local]--[hash:base64:5]', 'less-loader']
+        }),
         exclude: /node_modules/,
-      }, 
+      },
       {
         test: /\.less$/,
-        loaders: ['style-loader', 'css-loader', `less-loader?{"sourceMap":true,"modifyVars":${JSON.stringify(theme)}}`],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', `less-loader?{"sourceMap":true,"modifyVars":${JSON.stringify(theme)}}`],
+        }),
         exclude: /src/,
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: 'css-loader'
+        }),
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader'
+        loader: 'url-loader',
+        exclude: /node_modules/,
       }
     ],
   },

f0c47fb04a941321890a44032823d705e8cc4596
sangle7 <sanglewang@gmail.com>
2018-02-24 16:33:13 +0800

    modify homepage

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import './home.less'
 
 const Homepage = () => {
-  return (<h1> home here </h1>)
+  return (<div className="text-wrapper">
+    <h1>HNC database</h1>
+    <p><span>这里是介绍词，介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍，大约80个字。</span></p>
+  </div>)
 }
 
 

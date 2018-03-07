492c17035fb044f4eb056672dadac49b2c748d69
sangle <sanglewang@gmail.com>
2018-03-07 18:25:29 +0800

    delete obj

--- src/routes/datasets/cases/index.js
+++ src/routes/datasets/cases/index.js
@@ -3,39 +3,6 @@ import PropTypes from 'prop-types'
 import queryString from 'query-string'
 import { Breadcrumb, Tabs, DatasourceTable, StackedBarChart } from '../../../components'
 
-const a = {
-  "SampleID": "GSM2260037",
-  "PMID": "28433800",
-  "Author": "Mahimkar",
-  "Year": 2017,
-  "Data Type": "Expression",
-  "Dataset ID": "GSE85195",
-  "Patient ID": "Patient_OSCC_OC 1760",
-  "molecule": "RNA",
-  "Cell Type": "",
-  "Cell_line": "",
-  "anatomicSite": "ORAL CAVITY",
-  "age": "43",
-  "gender": "Male",
-  "race": "",
-  "Tumor/Normal": "Tumor",
-  "Tgrade": "",
-  "ClinicalStage": "2",
-  "Tstage": "",
-  "Nstage": "",
-  "Mstage": "",
-  "tobacco": "",
-  "alcohol": "",
-  "vital": "",
-  "hpv": "",
-  "recurrence": "",
-  "followUpMonths": "",
-  "survival": "",
-  "OS Time": "",
-  "PFS Time": "",
-  "source": "GEO\r"
-}
-
 const DatasetsCases = props => {
   const { location, history, dataSource, pagination, loading, chartSource } = props
   const BreadcrumbProps = {

a1ffa3061e15015cc7e791628ceae4c875af52ba
sangle <sanglewang@gmail.com>
2018-04-06 19:52:56 +0800

    replace nav width bs nav

--- src/routes/navigation.js
+++ src/routes/navigation.js
@@ -8,65 +8,56 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 const SubMenu = Menu.SubMenu;
 
 
-// class MMM extends React.Component {
-//   state = {
-//     scroll: false
-//   }
-//   componentDidMount () {
-//     /*监听滚动事件*/
-//     window.addEventListener('scroll',this.listenScroll);
-//   }
-//   listenScroll = () => {
-//     if(window.pageYOffset){
-//       this.setState({
-//         scroll:true
-//       })
-//     }else{
-//       this.setState({
-//         scroll:false
-//       })
-//     }
-//   }
-//   render () {
-//     const { history, location } = this.props
-//     return(
-//       <Row className={classnames({ [style.menu]: true ,[style.scroll]: this.state.scroll})}>
-//         <Menu
-//           id="nav"     
-//           onClick={(e)=>history.push(e.key)}
-//           selectedKeys={[location.pathname]}
-//           mode="horizontal">
-//           {menu.map(elem => elem.child ? <SubMenu title={elem.text}>
-//               {elem.child.map(e=><Menu.Item key={e.src}>{e.text}</Menu.Item>)}
-//           </SubMenu> :<Menu.Item key={elem.src}>{elem.text}</Menu.Item>)}
-//         </Menu>
-//       </Row>
-//     )
-//   }
-// }
+class MMM extends React.Component {
+  state = {
+    scroll: false
+  }
+  componentDidMount () {
+    /*监听滚动事件*/
+    window.addEventListener('scroll',this.listenScroll);
+  }
+  listenScroll = () => {
+    if(window.pageYOffset){
+      this.setState({
+        scroll:true
+      })
+    }else{
+      this.setState({
+        scroll:false
+      })
+    }
+  }
+  render () {
+    const { history, location } = this.props
+    // return(
+    //   <Row className={classnames({ [style.menu]: true ,[style.scroll]: this.state.scroll})}>
+    //     <Menu
+    //       id="nav"     
+    //       onClick={(e)=>history.push(e.key)}
+    //       selectedKeys={[location.pathname]}
+    //       mode="horizontal">
+    //       {menu.map(elem => elem.child ? <SubMenu title={elem.text}>
+    //           {elem.child.map(e=><Menu.Item key={e.src}>{e.text}</Menu.Item>)}
+    //       </SubMenu> :<Menu.Item key={elem.src}>{elem.text}</Menu.Item>)}
+    //     </Menu>
+    //   </Row>
+    // )
+    return (<Navbar className={classnames({ [style.menu]: true ,[style.scroll]: this.state.scroll})} collapseOnSelect onSelect={e=> history.push(e)}>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/">HNC database</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight activeKey={location.pathname}>
+        {menu.map(elem => elem.child ? <NavDropdown title={elem.text}>
+          {elem.child.map(e=><MenuItem eventKey={e.src}>{e.text}</MenuItem>)}
+        </NavDropdown> :<NavItem eventKey={elem.src}>{elem.text}</NavItem>)}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>)
+  }
+}
 
-
-
-const MMM = props => (<Navbar>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#home">React-Bootstrap</a>
-    </Navbar.Brand>
-  </Navbar.Header>
-  <Nav>
-    <NavItem eventKey={1} href="#">
-      Link
-    </NavItem>
-    <NavItem eventKey={2} href="#">
-      Link
-    </NavItem>
-    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-      <MenuItem eventKey={3.1}>Action</MenuItem>
-      <MenuItem eventKey={3.2}>Another action</MenuItem>
-      <MenuItem eventKey={3.3}>Something else here</MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey={3.4}>Separated link</MenuItem>
-    </NavDropdown>
-  </Nav>
-</Navbar>)
 export default MMM
